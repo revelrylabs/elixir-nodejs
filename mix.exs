27df517ab91aeb5fcd93404a4658806f03d97597
Bryan Joseph <bryanjos@gmail.com>
2018-06-30 14:53:07 -0500

    Changes for 2.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ReactRender.MixProject do
   def project do
     [
       app: :react_render,
-      version: "1.0.0",
+      version: "2.0.0-dev",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
--- publish.sh
+++
@@ -1,4 +0,0 @@
-mix deps.get
-mix hex.config username "$HEX_USERNAME"
-mix hex.config encrypted_key "$HEX_ENCRYPTED_KEY" > /dev/null 2>&1
-echo "$HEX_PASSPHRASE" | mix hex.publish --no-confirm
