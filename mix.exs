949614fd5d8fe579fd9a2c73b6426ba09e893b72
Hung Tran <hung@0x0.ninja>
2019-09-27 20:56:54 -0500

    update .tool-versions

--- mix.exs
+++ mix.exs
@@ -37,9 +37,9 @@ defmodule NodeJS.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:jason, "~> 1.0"},
       {:ex_doc, "~> 0.20.0", only: :dev},
       {:excoveralls, "~> 0.11.0", only: :test},
+      {:jason, "~> 1.0"},
       {:poolboy, "~> 1.5.1"}
     ]
   end
