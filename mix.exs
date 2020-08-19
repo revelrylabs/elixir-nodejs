dbdd275505c55feb932ca7d5b5d9f8585565a2f8
Hung Tran <hung@0x0.ninja>
2020-08-19 18:10:00 -0500

    bump to 2.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NodeJS.MixProject do
   def project do
     [
       app: :nodejs,
-      version: "1.1.0",
+      version: "2.0.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
