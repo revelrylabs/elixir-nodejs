85d87e4f7f7f147e5aa47c1bb6bd9b74b37539f1
Jason Pollentier <jason.pollentier@revelry.co>
2024-05-15 16:56:25 -0500

    wip: upgrade languages and libraries, move to GitHub actions

--- mix.exs
+++ mix.exs
@@ -38,9 +38,9 @@ defmodule NodeJS.MixProject do
   defp deps do
     [
       {:ex_doc, "~> 0.22.0", only: :dev},
-      {:excoveralls, "~> 0.13.0", only: :test},
       {:jason, "~> 1.0"},
-      {:poolboy, "~> 1.5.1"}
+      {:poolboy, "~> 1.5.1"},
+      {:ssl_verify_fun, "~> 1.1.7", manager: :rebar3, override: true}
     ]
   end
 
