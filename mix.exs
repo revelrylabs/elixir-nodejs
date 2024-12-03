522d29c05814d993f4f6255614e943a867bc2201
Jason Pollentier <jason.pollentier@revelry.co>
2024-12-02 17:11:30 -0700

    chore: bump version 3.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NodeJS.MixProject do
   def project do
     [
       app: :nodejs,
-      version: "3.1.0",
+      version: "3.1.1",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
